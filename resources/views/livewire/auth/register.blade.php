549a05b264cf7de67c4274cd9a4b9e64f157fcc4
Paul Frederik de Zwaan <pfzwaan@gmail.com>
2025-03-11 12:01:23 -0600

    login & register

--- resources/views/livewire/auth/register.blade.php
+++ resources/views/livewire/auth/register.blade.php
@@ -8,7 +8,7 @@ use Illuminate\Validation\Rules;
 use Livewire\Attributes\Layout;
 use Livewire\Volt\Component;
 
-new #[Layout('components.layouts.auth')] class extends Component {
+new #[Layout('layouts.site')] class extends Component {
     public string $name = '';
     public string $email = '';
     public string $password = '';
@@ -35,60 +35,102 @@ new #[Layout('components.layouts.auth')] class extends Component {
     }
 }; ?>
 
-<div class="flex flex-col gap-6">
-    <x-auth-header title="Create an account" description="Enter your details below to create your account" />
+<div class="main-container relative w-full overflow-hidden">
+    <section class="section section--hero-interior bg-primaryLight relative py-[140px]">
+        <img class="absolute bottom-0 left-0 z-0" src="{{ asset('dieren/src/public/img/about1.png') }}" alt="">
+        <div class="container mx-auto relative z-1">
+            <h1 class="text-6xl text-center font-regular text-gray-800 relative z-1">
+                Registreren
+            </h1>
+        </div>
+        <img class="absolute bottom-0 right-0 z-0 hidden lg:block" src="{{ asset('dieren/src/public/img/contact.png') }}" alt="">
+    </section>
 
-    <!-- Session Status -->
-    <x-auth-session-status class="text-center" :status="session('status')" />
+    <section class="section section--login pb-[40px]">
+        <div class="container mx-auto">
+            <div class="grid grid-cols-1 lg:grid-cols-12">
+                <div class="lg:col-span-8">
 
-    <form wire:submit="register" class="flex flex-col gap-6">
+
+    <form wire:submit="register" class="px-[50px] py-[60px] bg-[#202428] lg:h-full">
+        <x-auth-header title="Create an account" description="Enter your details below to create your account" />
+
+        <!-- Session Status -->
+        <x-auth-session-status class="text-center" :status="session('status')" />
         <!-- Name -->
         <div class="grid gap-2">
-            <flux:input wire:model="name" id="name" label="{{ __('Name') }}" type="text" name="name" required autofocus autocomplete="name" placeholder="Full name" />
+            <flux:field class="my-5">
+                <flux:label class="text-white mb-2">{{ __('Name') }}</flux:label>
+
+                <flux:input wire:model="name" id="name" type="text" name="name" required autofocus autocomplete="name" placeholder="Full name" />
+
+                <flux:error name="name" />
+            </flux:field>
         </div>
 
         <!-- Email Address -->
         <div class="grid gap-2">
-            <flux:input wire:model="email" id="email" label="{{ __('Email address') }}" type="email" name="email" required autocomplete="email" placeholder="email@example.com" />
+            <flux:field class="my-5">
+                <flux:label class="text-white mb-2">{{ __('Email address') }}</flux:label>
+                <flux:input wire:model="email" id="email" type="email" name="email" required autocomplete="email" placeholder="email@example.com" />
+                <flux:error name="email" />
+            </flux:field>
         </div>
 
         <!-- Password -->
         <div class="grid gap-2">
-            <flux:input
-                wire:model="password"
-                id="password"
-                label="{{ __('Password') }}"
-                type="password"
-                name="password"
-                required
-                autocomplete="new-password"
-                placeholder="Password"
-            />
+            <flux:field class="my-5">
+                <flux:label class="text-white mb-2">{{ __('Password') }}</flux:label>
+                <flux:input
+                    wire:model="password"
+                    id="password"
+                    type="password"
+                    name="password"
+                    required
+                    autocomplete="new-password"
+                    placeholder="Password"
+                />
+                <flux:error name="password" />
+            </flux:field>    
         </div>
 
         <!-- Confirm Password -->
         <div class="grid gap-2">
-            <flux:input
-                wire:model="password_confirmation"
-                id="password_confirmation"
-                label="{{ __('Confirm password') }}"
-                type="password"
-                name="password_confirmation"
-                required
-                autocomplete="new-password"
-                placeholder="Confirm password"
-            />
+            <flux:field class="my-5">
+                <flux:label class="text-white mb-2">{{ __('Confirm password') }}</flux:label>
+                <flux:input
+                    wire:model="password_confirmation"
+                    id="password_confirmation"
+                    type="password"
+                    name="password_confirmation"
+                    required
+                    autocomplete="new-password"
+                    placeholder="Confirm password"
+                />
+                <flux:error name="password_confirmation" />
+            </flux:field>   
+
         </div>
 
-        <div class="flex items-center justify-end">
-            <flux:button type="submit" variant="primary" class="w-full">
+        <div class="flex items-center mt-5 justify-end">
+            <flux:button type="submit" variant="primary" class="btn btn-primaryLight w-full rounded-[5px] text-white">
                 {{ __('Create account') }}
             </flux:button>
         </div>
-    </form>
+        </div>
+                <div class="lg:col-span-4 bg-gray-200">
+                    <figure class="h-full">
+                        <img class="h-full w-full object-cover" src="{{ asset('dieren/src/public/img/contact2.png') }}" alt="">
+                    </figure>
+                </div>
+            </div>
+        </div>
+    </section>
 
-    <div class="space-x-1 text-center text-sm text-zinc-600 dark:text-zinc-400">
-        Already have an account?
-        <x-text-link href="{{ route('login') }}">Log in</x-text-link>
-    </div>
+    <section class="section section--info pt-[40px] pb-0 text-center">
+        <div class="container mx-auto">
+        <p class="text-gray-800 text-lg">Already have an account?
+            <a href="{{ route('login') }}" class="text-primary font-bold">Log in</a></p>
+        </div>
+    </section>
 </div>
