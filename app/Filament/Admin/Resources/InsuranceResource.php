5cfe7eabd4e940a377958fb2b5329eddcf6d3005
Paul Frederik de Zwaan <pfzwaan@gmail.com>
2025-03-11 09:55:32 -0600

    insurances

--- app/Filament/Admin/Resources/InsuranceResource.php
+++ app/Filament/Admin/Resources/InsuranceResource.php
@@ -6,9 +6,11 @@ use App\Filament\Admin\Resources\InsuranceResource\Pages;
 use App\Filament\Admin\Resources\InsuranceResource\RelationManagers;
 use App\Models\Insurance;
 use Filament\Forms;
+use Filament\Forms\Components\Section;
 use Filament\Forms\Form;
 use Filament\Resources\Resource;
 use Filament\Tables;
+use Filament\Tables\Actions\ActionGroup;
 use Filament\Tables\Table;
 use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\SoftDeletingScope;
@@ -17,27 +19,66 @@ class InsuranceResource extends Resource
 {
     protected static ?string $model = Insurance::class;
 
-    protected static ?string $navigationIcon = 'heroicon-o-rectangle-stack';
+    protected static ?string $navigationIcon = 'heroicon-o-document-currency-dollar';
+
+    protected static ?string $navigationGroup = 'Content management';
+
+    protected static ?int $navigationSort = 6;
 
     public static function form(Form $form): Form
     {
         return $form
             ->schema([
-                //
+                Section::make('Insurances') // Title of the section
+                    ->schema([
+                        Forms\Components\FileUpload::make('image')
+                            ->image(),
+                        Forms\Components\TextInput::make('title')
+                            ->required()
+                            ->maxLength(255),
+                        Forms\Components\TextInput::make('discount')
+                            ->required(),
+                        Forms\Components\Textarea::make('small_description')
+                            ->columnSpanFull(),
+                        Forms\Components\TextInput::make('link')
+                            ->maxLength(255)
+                            ->url()
+                            ->helperText('Enter a valid URL, e.g., https://example.com'),
+                ]),
             ]);
-    }
+        }
 
     public static function table(Table $table): Table
     {
         return $table
+            ->defaultSort('id', 'desc')
             ->columns([
-                //
+                Tables\Columns\TextColumn::make('title')
+                    ->searchable(),
+                Tables\Columns\ImageColumn::make('image'),
+                Tables\Columns\TextColumn::make('discount')
+                    ->numeric()
+                    ->sortable(),
+                Tables\Columns\TextColumn::make('link')
+                    ->searchable(),
+                Tables\Columns\TextColumn::make('created_at')
+                    ->dateTime()
+                    ->sortable()
+                    ->toggleable(),
+                Tables\Columns\TextColumn::make('updated_at')
+                    ->dateTime()
+                    ->sortable()
+                    ->toggleable(isToggledHiddenByDefault: true),
             ])
             ->filters([
                 //
             ])
             ->actions([
-                Tables\Actions\EditAction::make(),
+                ActionGroup::make([
+                    Tables\Actions\ViewAction::make(),
+                    Tables\Actions\EditAction::make(),
+                    Tables\Actions\DeleteAction::make(),
+                ])
             ])
             ->bulkActions([
                 Tables\Actions\BulkActionGroup::make([
