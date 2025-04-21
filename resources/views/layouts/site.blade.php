74af2025332ee6d789083ec6f0ac12e88fad7bb2
Paul Frederik de Zwaan <pfzwaan@gmail.com>
2025-04-21 09:14:50 -0600

    test

--- resources/views/layouts/site.blade.php
+++ resources/views/layouts/site.blade.php
@@ -5,7 +5,8 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta name="csrf-token" content="{{ csrf_token() }}">
     <title>{!! $seo['title'] ?? 'Default Title' !!}</title>
-
+    <link rel="shortcut icon" href="/favicon.ico" />
+    <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
     <!-- Meta description -->
     <meta name="description" content="{!! $seo['description'] ?? 'Default description' !!}">
 
