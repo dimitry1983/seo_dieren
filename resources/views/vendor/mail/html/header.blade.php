f3e0490091163fe017e3471aaabcb334421a4a16
Paul Frederik de Zwaan <pfzwaan@gmail.com>
2025-04-21 10:42:19 -0600

    logo inlog cms

--- resources/views/vendor/mail/html/header.blade.php
+++ resources/views/vendor/mail/html/header.blade.php
@@ -3,9 +3,9 @@
 <td class="header">
 <a href="{{ $url }}" style="display: inline-block;">
 @if (trim($slot) === 'Laravel')
-<img src="http://localhost:8000/dieren/src/public/img/Logo.png" class="logo" alt="Laravel Logo">
+<img src="http://localhost:8000/dieren/src/public/img/logo.png" class="logo" alt="Laravel Logo">
 @else
-<img src="http://localhost:8000/dieren/src/public/img/Logo.png" class="logo" style="width:100%;max-width:320px;height:auto;" alt="Laravel Logo">
+<img src="http://localhost:8000/dieren/src/public/img/logo.png" class="logo" style="width:100%;max-width:320px;height:auto;" alt="Laravel Logo">
 @endif
 </a>
 </td>
