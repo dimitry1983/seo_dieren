47eec49557ecc5a569038eb1cdcbd3fd34f1c2ab
Paul Frederik de Zwaan <pfzwaan@gmail.com>
2025-04-21 10:29:27 -0600

    mobile improvements

--- resources/views/website/interior-blog.blade.php
+++ resources/views/website/interior-blog.blade.php
@@ -35,29 +35,32 @@
                             </h4>
                             @if (!empty($blogs))
                             @foreach($blogs as $related)
-                                <div class="article relative border border-gray-300 transition duration-300 ease-out shadow-lg mb-3">
-                                    <div class="grid grid-cols-5 lg:grid-cols-5 gap-4">
-                                        <figure class="col-span-2 lg:col-span-2 mb-0 h-full">
-                                            <div class="w-full h-full bg-cover bg-center"
-                                                style="background-image: url('{{ $related->thumb ? Storage::url($related->thumb) : asset('dieren/src/public/img/article1.jpg') }}');">
-                                            </div>
-                                        </figure>
-                                        <div class="col-span-3 lg:col-span-3 pr-4 py-2 relative">
-                                            <h3 class="title title--article text-normal font-bold">
-                                                <a href="{{ route('blog.detail', ['slug' => Str::slug($related->name), 'id' => $related->id]) }}">
-                                                    {{ Str::limit($related->name, 50) }}
-                                                </a>
-                                            </h3>
-                                            <p class="text-sm mb-0">
-                                                {{ \Illuminate\Support\Str::limit(strip_tags($related->description), 60) }}
-                                            </p>
-                                            <a href="{{ route('blog.detail', ['slug' => Str::slug($related->name), 'id' => $related->id]) }}"
-                                            class="text-primary absolute lg:right-[8px] right-[15px] lg:bottom-[5px] bottom-[15px] text-lg">
-                                                <i class="fa-sharp fa-solid fa-circle-arrow-right"></i>
-                                            </a>
+                            <div class="article relative border border-gray-300 transition duration-300 ease-out shadow-lg mb-3">
+                                <div class="grid grid-cols-1 lg:grid-cols-5 gap-4">
+                                    <!-- Image section -->
+                                    <figure class="lg:col-span-2 mb-0 h-full">
+                                        <div class="w-full h-full bg-cover bg-center aspect-[3/2] lg:aspect-auto"
+                                            style="background-image: url('{{ $related->thumb ? Storage::url($related->thumb) : asset('dieren/src/public/img/article1.jpg') }}');">
                                         </div>
+                                    </figure>
+
+                                    <!-- Text section -->
+                                    <div class="lg:col-span-3 pr-4 py-2 px-2 md:px-0 relative">
+                                        <h3 class="title title--article text-normal font-bold">
+                                            <a href="{{ route('blog.detail', ['slug' => Str::slug($related->name), 'id' => $related->id]) }}">
+                                                {{ Str::limit($related->name, 50) }}
+                                            </a>
+                                        </h3>
+                                        <p class="text-sm mb-0">
+                                            {{ \Illuminate\Support\Str::limit(strip_tags($related->description), 60) }}
+                                        </p>
+                                        <a href="{{ route('blog.detail', ['slug' => Str::slug($related->name), 'id' => $related->id]) }}"
+                                        class="text-primary absolute lg:right-[8px] right-[15px] lg:bottom-[5px] bottom-[15px] text-lg">
+                                            <i class="fa-sharp fa-solid fa-circle-arrow-right"></i>
+                                        </a>
                                     </div>
                                 </div>
+                            </div>
                             @endforeach
                             @endif
                         </div>
